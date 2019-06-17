8795d427b1d13c7eefec56f1475dfe58eff8cd9e
nagarjundeepak <deepaknagarjun@gmail.com>
2019-06-17 22:53:56 +0530

    changes

--- news-raven/src/components/containers/posts.js
+++ news-raven/src/components/containers/posts.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 const Posts = (props) => {
     const data = { ...props.data };
+    console.log(data[0]);
     const titles = [];
     Object.keys(data).map((key, index) => {
         return (titles[key] = (
           <div className="job" key={data[key].title}>
+            <img alt='' src={data[key].urlToImage} />
             <span>{data[key].title}</span>
+            <a href={data[key].url} target='_blank'>read more</a>
           </div>
         ));
       });
